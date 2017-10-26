10311cde14c6b9153cb083f7aab759dd356d2b3a
Unknown <aironskythe@gmail.com>
2017-10-26 19:43:14 +0300

    add game "guess word"

---
+++ гра ''вгадай слово''/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class BP
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int secret = rand.Next(1, 11);
+        int gusca = 0;
+        while (secret != gusca)
+        {
+            gusca = Convert.ToInt32(Console.ReadLine());
+            if (secret < gusca)
+            {
+                Console.WriteLine("Неправильно! секрт менший за введене число");
+            }
+            if (secret > gusca)
+            {
+                Console.WriteLine("Неправильно! секрт більший за введене число");
+            }
+        }
+            Console.WriteLine("Правильно!" + "Секретом був "+secret);
+            Console.ReadKey();
+    }
+}
+
+
