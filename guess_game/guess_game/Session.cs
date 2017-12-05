9788dbaf288ed1139203306705aace3c179ba562
Unknown <aironskythe@gmail.com>
2017-12-05 16:38:23 +0200

    - add DAO
    - changed some objects

---
+++ guess_game/guess_game/Session.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace guess_word_game
+{
+    class Session
+    {
+        public List<Container> Tests;
+        Container Current;
+        List<int> usedQuests;
+        public string PlayerName;
+        public int Score
+        {
+            get;
+            private set;
+        }
+  
+        public Session()
+        {
+            usedQuests = new List<int>();
+            Tests = new List<Container>();
+            var fs = new StreamReader(@"C:\Users\Skythe\Desktop\решта\Візуал проекти\вгадай слово\слова\secret.txt");
+            while (!fs.EndOfStream)
+            {
+                Tests.Add(   new Container ( fs.ReadLine().Split('-') )   );
+            }
+        }
+
+        public Container GetQuest()
+        {
+            Random rand = new Random();
+            int index;
+            do
+            {
+                index = rand.Next(0, Tests.Count);
+            }
+            while (usedQuests.Contains(index));
+
+            usedQuests.Add(index);
+            Current = Tests[index];
+            return Tests[index];
+        }
+        public bool IsWright(Container s)
+        {
+            if (s.Answer == Current.Answer)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
